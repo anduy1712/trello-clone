e7cb746e17bb7ce6e19210467b57b8e516aca504
Phan Tran Duy An <duyan@MacBook-Air-cua-Phan.local>
2021-08-11 10:30:07 +0700

    Create state, edit ui

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -3,19 +3,16 @@ import React from 'react';
 import { Container, Draggable } from 'react-smooth-dnd';
 import { mapOrder } from 'utilities/sort';
 
-const Column = ({ column }) => {
+const Column = ({ column, onCardDrop }) => {
   const cards = mapOrder(column.cards, column.cardOrder, 'id');
 
-  const onCardDrop = (dropResult) => {
-    console.log(dropResult);
-  };
   return (
     <div className="column">
       <div className="column-header column-drag-handle">{column.title}</div>
       <div className="column-list">
         <Container
           groupName="col"
-          onDrop={onCardDrop}
+          onDrop={(dropResult) => onCardDrop(column.id, dropResult)}
           getChildPayload={(index) => cards[index]}
           dragClass="card-ghost"
           dropClass="card-ghost-drop"
@@ -35,7 +32,11 @@ const Column = ({ column }) => {
           })}
         </Container>
       </div>
-      <div className="column-footer">Duy An</div>
+      <div className="column-footer">
+        <div className="footer-actions">
+          <i className="fa fa-plus icon" /> Add Card
+        </div>
+      </div>
     </div>
   );
 };
